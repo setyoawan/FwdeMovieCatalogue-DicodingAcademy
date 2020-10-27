f66439438e4c96c1958a2559bda92b425c996957
setyoawan <setyoawan8@gmail.com>
2020-10-27 08:55:23 +0700

    initial commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: '4c69d8488e01060e294622637453e829',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+};
+
+export default CONFIG;
